a23720483a42522b5dc9387351e09cb0ab758f07
Dimko <dimko@ubuntu.(none)>
2012-10-18 15:00:44 +0200

    PlayerFactory added

---
+++ src/player/PlayerFactory.java
@@ -0,0 +1,32 @@
+package player;
+
+import player.evaluation.TestEvaluation;
+
+public class PlayerFactory {
+
+	public static IPlayer createPlayer(String[] args, int i) {
+		if (args[i].trim().equals("human")) {
+			return new HumanPlayer();
+		}
+		else if (args[i].trim().equals("random")) {
+			return new RandomPlayer();
+		}
+		else if (args[i].trim().equals("novice")) {
+			return new NovicePlayer();
+		}
+		else if (args[i].trim().equals("mc")) {
+			int simulations = Integer.parseInt(args[++i]);
+			return new MonteCarloPlayer(simulations);
+		}
+		else if (args[i].trim().equals("pmc")) {
+			int simulations = Integer.parseInt(args[++i]);
+			return new MonteCarloParallelPlayer(simulations);
+		}
+		else if (args[i].trim().equals("minmax")) {
+			int maxDepth = Integer.parseInt(args[++i]);
+			return new MinMaxAlphaBetaPlayer(maxDepth, new TestEvaluation());
+		}
+		return null;
+	}
+
+}
